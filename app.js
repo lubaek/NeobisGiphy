7eb556eaa55881ea92dd1a5b80b711aa986688dd
lubaek <60066060+lubaek@users.noreply.github.com>
2020-10-27 19:36:37 +0600

    created index.html & app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const gifs = document.querySelector('.gifs');
+searchForm.addEventListener('submit', e=> {
+    e.preventDefault();
+    const queryString = searchInput.value;
+    searchGif(queryString);
+})
+
+function searchGif(queryString) {
+    const apikey = 'bZmGhDit6ReSZCGtOUwUnKIKkRCJxGcE';
+    const path = `http://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${queryString}`;
+    fetch(path).then(res=> {
+        return res.json();
+    }).then(json =>{
+        let imgHTML = '';
+        json.data.forEach(obj => {
+            // console.log(obj.images.fixed_width.url);
+            const url = obj.images.fixed_width.url;
+            const title = obj.title;
+            imgHTML+=  `<img src="${url}" alt="${title}">`
+        });
+        gifs.innerHTML = imgHTML;
+    }).catch(err => {
+        console.log(err.message);
+    })
+}
+
+
+
